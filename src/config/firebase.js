fafa7987a8743383acd30c650f63f92ed0c1e6ca
Khoi Dang <tuankhoionline@gmail.com>
2025-12-09 00:55:35 +0700

    env for Security

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Using environment variables for security
 const firebaseConfig = {
-  apiKey: "AIzaSyArQz5pQDQUuqhivQIa5hpLiD37AYP5CyA",
-  authDomain: "travelapp-fa9c9.firebaseapp.com",
-  projectId: "travelapp-fa9c9",
-  storageBucket: "travelapp-fa9c9.firebasestorage.app",
-  messagingSenderId: "268639454818",
-  appId: "1:268639454818:web:48430d0a0a9ac08592333b",
-  measurementId: "G-H4EEQ8XN6Z"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 
